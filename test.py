269b61b61e286f45a10949feddbd99ba3a027b12
Techno263 <techno263code@gmail.com>
2021-01-23 13:24:47 -0500

    remove mc data submodule

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-import minecraft_data.minecraft_data as mcd
+import minecraft_data as mcd
 import json
 
 if __name__ == '__main__':
     with open('protocol_data.json', 'wt') as fp:
-        breakpoint()
         mc_data = mcd("1.16.4")
         json.dump(mc_data.protocol, fp)
